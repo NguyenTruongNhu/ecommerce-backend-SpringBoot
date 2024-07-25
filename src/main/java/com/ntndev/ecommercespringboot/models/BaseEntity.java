a21ad9dec2ccae3e63ecbac0c8dd17717f56395d
NguyenTruongNhu <nhu81632@gmail.com>
2024-07-25 13:44:08 +0700

    create model product, category, setup baseEntity(createdAt,updatedAt)

---
+++ src/main/java/com/ntndev/ecommercespringboot/models/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.ntndev.ecommercespringboot.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}
---
+++ src/main/java/com/ntndev/ecommercespringboot/models/Product.java
@@ -0,0 +1,40 @@
+package com.ntndev.ecommercespringboot.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name",nullable = false, length = 350)
+    private String name;
+
+    private Float price;
+
+    @Column(name = "thumbnail",length = 350)
+    private String thumbnail;
+
+    @Column(name = "description")
+    private String description;
+
+
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+
+
+
+
+}
