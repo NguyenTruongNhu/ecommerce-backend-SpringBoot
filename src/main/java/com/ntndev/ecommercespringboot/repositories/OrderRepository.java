185eee1821280378e517c6fbe34be71b0d1f49ae
NguyenTruongNhu <nhu81632@gmail.com>
2024-07-25 14:33:22 +0700

    done create repository

---
+++ src/main/java/com/ntndev/ecommercespringboot/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.ntndev.ecommercespringboot.repositories;
+
+import com.ntndev.ecommercespringboot.models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+    List<Order> findByUserId(Long userId);
+}
---
+++ src/main/java/com/ntndev/ecommercespringboot/repositories/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.ntndev.ecommercespringboot.repositories;
+
+import com.ntndev.ecommercespringboot.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
